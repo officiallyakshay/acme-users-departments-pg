7bc5c4d016b3f819f2ed4ea930c5e29857ffe7db
casualshay <akshaysprabhakar@gmail.com>
2019-09-16 14:02:11 -0700

    progress

--- db.js
+++ db.js
@@ -2,6 +2,57 @@ const pg = require('pg');
 const { Client } = pg;
 
 const uuid = require('uuid');
-const client = new Client('postgres://localhost/acme_users_department');
+const client = new Client('postgres://localhost/the_acme_db');
 
 client.connect();
+
+const SQL = `
+  DROP TABLE IF EXISTS users;
+  DROP TABLE IF EXISTS departments;
+  
+  CREATE TABLE departments(
+    id INT PRIMARY KEY,
+    name VARCHAR(255) UNIQUE NOT NULL
+  );
+
+  CREATE TABLE users(
+    id INT PRIMARY KEY,
+    name VARCHAR(255) UNIQUE NOT NULL,
+    department_id INT REFERENCES departments(id)
+  );
+
+  INSERT INTO departments(id, name)
+  values('12345', 'Accounting');
+  INSERT INTO departments(id, name)
+  values('12346', 'Marketing');
+
+  INSERT INTO users(id, name)
+  values('5678', 'Howard');
+  INSERT INTO users(id, name)
+  values('7890', 'Jimmy');
+
+`
+const sync = async () => {
+  try {
+    await client.query(SQL);
+  }
+  catch(ex) {
+    console.log(ex.message);
+  }
+};
+
+const findAllUsers = async () => {
+  const response = await client.query('Select * FROM users;');
+  return response.rows;
+}
+
+const findAllDepartments = async () => {
+  const response = await client.query('Select * FROM departments;');
+  return response.rows;
+}
+
+module.exports = {
+  sync,
+  findAllUsers,
+  findAllDepartments
+}
--- server.js
+++ server.js
@@ -7,20 +7,22 @@ app.get('/', (req, res, next) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.get('/api/users', (req, res, next) => {
-
+app.get('/api/users', async (req, res, next) => {
+  try {
+    res.send(await db.findAllUsers());
+  }
+  catch(ex) {
+    next(ex);
+  }
 });
 
-app.post('/api/users', (req, res, next) => {
-  
-});
-
-app.get('/api/departments', (req, res, next) => {
-  
-});
-
-app.post('/api/departments', (req, res, next) => {
-
+app.get('/api/departments', async (req, res, next) => {
+  try {
+    res.send(await db.findAllDepartments());
+  }
+  catch(ex) {
+    next(ex);
+  }
 });
 
 db.sync()
